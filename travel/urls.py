3d13f0c8c262f5682cd09783066d07512845a0a3
khushi <khushi@gmail.com>
2024-02-12 22:46:22 +0530

    second

--- travel/urls.py
+++ travel/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("",views.index,name='index')
+    path('register',views.register_user,name='register_user'),
+    path('login',views.login_user,name='login_user'),
+    path('logout',views.logout_user,name='logout_user'),
+    path('',views.index,name='index')
 ]
+
--- travel/views.py
+++ travel/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages 
+from .forms import SignUpForm
 from .models import Destination
+
+def register_user(request):
+    if request.method=='POST':
+        form=SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+            user=authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,"you have been successfully registerd ")
+            return redirect('login_user')
+    else:
+        form=SignUpForm()
+        return render(request,'register.html',{'form':form})
+    return render(request,'register.html',{'form':form})  
+
+def login_user(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"You have been logged in")
+            return redirect('/')
+        else:
+            messages.success(request,"Invalid Credentails")
+    else:
+        return render(request,'login.html')   
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"You Have Been Logged Out..")
+    return redirect('login_user')           
 
 def index(request):
     dests=Destination.objects.all()
